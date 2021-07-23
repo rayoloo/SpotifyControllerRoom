c89868dfccfaf2894b6cbfdd1571252b2742b384
rayoloo <43391310+rayoloo@users.noreply.github.com>
2021-07-23 02:57:12 -0400

    july 23

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <h1>Testing React 1 Code!</h1>
+        )
+    }
+}
+
---
+++ music_controller/frontend/src/index.js
@@ -0,0 +1,6 @@
+import App from "./components/App"
+import React from 'react'
+import {render} from 'react-dom'
+
+const appDiv = document.getElementById("root")
+render(<App/>, appDiv)
