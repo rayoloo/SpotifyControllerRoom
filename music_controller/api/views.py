e1f7d055069660f483d0d20cfe9ca3a3a044de30
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-08-11 22:37:44 -0400

    aug 11

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Create your views here.
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all() # pylint: disable=maybe-no-member
     serializer_class = RoomSerializer
 
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, form=None):
+        pass
