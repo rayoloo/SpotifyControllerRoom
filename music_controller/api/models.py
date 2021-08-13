5a28cf28a6688f1effafc188a13c727969a5d360
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-08-13 03:26:03 -0400

    aug 13

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -2,20 +2,20 @@ from django.db import models
 import string
 import random
 
+
 def generate_unique_code():
     length = 6
 
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if Room.objects.filter(code=code).exists() : # pylint: disable=maybe-no-member
+        if Room.objects.filter(code=code).exists(): # pylint: disable=maybe-no-member
             break
 
     return code
 
-# Create your models here.
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
