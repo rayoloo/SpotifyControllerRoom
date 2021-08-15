2c3822dd62f51865cd05cdb360b93ebe64700ad0
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-08-14 23:37:09 -0400

    aug 14

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
+import {
+    Button, 
+    Grid, 
+    Typography, 
+    Textfield, 
+    FormControl,
+    Radio,
+    RadioGroup,
+    FormControlLabel
+    } from '@material-ui/core';
+import {Link} from 'react-router-dom';
+
 
 export default class CreateRoomPage extends Component {
+    defaultVote = 2
+
     constructor(props) {
         super(props)
     }
 
     render() {
         return (
-            <p>This is the create room page.</p>
+            // 1 x 8 px spacing between the grid
+            //grid container
+            <Grid container spacing={1}> 
+                <Grid item xs={12} align="center"></Grid>
+                {/* if you want to fill the width of the grid make it xs=12 since that the maximum number for this value*/}
+                    <Typography component='h4' variant='h4'>
+                        Create a room
+                    </Typography>
+            </Grid>
         )
     }
 }
