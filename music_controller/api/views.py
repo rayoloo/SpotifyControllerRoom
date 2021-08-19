b3618b440847b898936ff826fe275ef5f36b3ef4
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-08-19 18:52:57 -0400

    aug 19

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,6 +5,7 @@ from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 # Create your views here.
 
 class RoomView(generics.ListAPIView):
@@ -18,26 +19,23 @@ class CreateRoomView(APIView):
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
-            # if there is no previous session create a new session
 
         serializer = self.serializer_class(data=request.data)
-        # get the data from the form that contains guest_can_pause and votes_to_skip
         if serializer.is_valid():
             guest_can_pause = serializer.data.get('guest_can_pause')
             votes_to_skip = serializer.data.get('votes_to_skip')
             host = self.request.session.session_key
-            # GET guest_can_pause and votes_to_skip information from the request
             queryset = Room.objects.filter(host=host) # pylint: disable=maybe-no-member
-            # find previous active rooms
-            if queryset.exists(): # update old room if found
+            if queryset.exists():
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
-            else: #create a new room 
-                room = Room(host=host, guest_can_pause=guest_can_pause,votes_to_skip=votes_to_skip)
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
                 room.save()
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
-        return Response({'Bad Request': 'Invalid Data'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -18,43 +18,92 @@ export default class CreateRoomPage extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes
+        }
+
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this)
+        this.handleVotesChange = this.handleVotesChange.bind(this)
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this)
+
+    }
+
+    handleVotesChange(e) {
+        this.setState({votesToSkip: e.target.value})
+    }
+
+    handleGuestCanPauseChange(e) {
+        this.setState({guestCanPause: e.target.value === "true" ? true : false})
+    }
+
+    handleRoomButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause
+            })
+        }
+        fetch("/api/create-room",requestOptions)
+            .then((response)=> response.json()) 
+            .then((data) => console.log(data))
     }
 
     render() {
         return (
-            // 1 x 8 px spacing between the grid
             //grid container
             <Grid container spacing={1}> 
                 <Grid item xs={12} align="center">
-                    {/* if you want to fill the width of the grid make it xs=12 since that the maximum size*/}
+                    {/*To fill the width of the grid make xs=12 since that the maximum size to take up the entire width*/}
                     <Typography component='h4' variant='h4'>
                         Create A Room
                     </Typography>
                 </Grid>
                 <Grid item xs={12} align="center">
                     <FormControl component="fieldset">
-                        <FormHelperText>
+                        <FormHelperText component="div">
                             <div align="center">
                                 Guest Control of Playback State
                             </div>
                         </FormHelperText>
-                        <RadioGroup row defaultValue="true">
-                            <FormControlLabel value="true" control={<Radio color="primary"/>} label="Play/Pause" labelPlacement="bottom"/>
-                            <FormControlLabel value="false" control={<Radio color="secondary"/>} label="Play/Pause" labelPlacement="bottom"/>
+                        <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
+                            <FormControlLabel 
+                                value="true" 
+                                control={<Radio color="primary"/>} 
+                                label="Play/Pause" 
+                                labelPlacement="bottom"
+                            />
+                            <FormControlLabel 
+                                value="false" 
+                                control={<Radio color="secondary"/>} 
+                                label="No Control" 
+                                labelPlacement="bottom"
+                            />
                         </RadioGroup>
                     </FormControl>
                 </Grid>
                 <Grid item xs={12} align="center">
                     <FormControl>
-                        <TextField required={true} type="number" defaultValue={this.defaultVotes} inputProps={{min:1, style: {textAlign: 'center'}}} />
-                        <FormHelperText>
+                        <TextField 
+                            required={true} 
+                            type="number" 
+                            defaultValue={this.defaultVotes} 
+                            inputProps={{min:1, style: {textAlign: 'center'}}}
+                            onChange={this.handleVotesChange} 
+                        />
+                        <FormHelperText component="div">
                             <div align="center">Votes Required To Skip Song</div>
                         </FormHelperText>
                     </FormControl>
                 </Grid>
                 <Grid item xs={12} align="center">
-                    
-                </Grid>  
+                    <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed}>Create A Room</Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" to="/" component={Link}>Back</Button>
+                </Grid>    
             </Grid>
         )
     }
